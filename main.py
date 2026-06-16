d470ef5d65867697955cae220b63716c56a50bec
Robert Vari <mail.robertvari@gmail.com>
2026-06-16 13:27:28 +0200

    Game class

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import pyray as RL
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 450
-TITLE = "Drone Fighter"
 
-def main():
-    RL.init_window(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE)
-    RL.set_target_fps(60)
+class DroneFighter:
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 450
+    TITLE = "Drone Fighter"
 
-    while not RL.window_should_close():
+    # Init game window  
+    def __init__(self):
+        print("Init window")
+
+        RL.init_window(self.SCREEN_WIDTH, self.SCREEN_HEIGHT, self.TITLE)
+        RL.set_target_fps(60)
+    
+    # update
+    def update(self):
+        pass
+
+    # draw
+    def draw(self):
         RL.begin_drawing()
 
         RL.clear_background(RL.BLACK)
-        RL.draw_text(TITLE, 280, 200, 40, RL.BLUE)
+        RL.draw_text(self.TITLE, 280, 200, 40, RL.WHITE)
 
         RL.end_drawing()
 
-    RL.close_window()
+    # exit game
+    def exit_game(self):
+        RL.close_window()
+    
+    # start game
+    def run(self):
+        while not RL.window_should_close():
+            self.update()
+            self.draw()
+        self.exit_game()
 
 
-# Start window
-main()
+game = DroneFighter()
+game.run()
