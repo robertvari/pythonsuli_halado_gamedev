845f4b775d74297f6f66479dc9453ddd365fe96d
Robert Vari <mail.robertvari@gmail.com>
2026-06-18 11:15:54 +0200

    Resource Manager

---
+++ ResourceManager.py
@@ -0,0 +1,31 @@
+import pyray as RL
+import os
+
+class ResourceManager:
+    RESOURCE_DIR = os.path.join(os.path.dirname(__file__), "resources")
+    TEXTURE_EXTENSIONS = {".png"}
+
+    def __init__(self):
+        # type annotation
+        self._textures: dict[str, RL.Texture2D] = {}
+        self._load_texture()
+
+    # private/protected methods
+    def _load_texture(self):
+        for filename in os.listdir(self.RESOURCE_DIR):
+            name, ext = os.path.splitext(filename)
+            if ext.lower() in self.TEXTURE_EXTENSIONS:
+                path = os.path.join(self.RESOURCE_DIR, filename)
+                self._textures[name] = RL.load_texture(path)
+    
+    # Public methods
+    def get_texture(self, name: str):
+        if name not in self._textures:
+            raise KeyError(f"Texture {name} not found")
+        return self._textures[name]
+
+    def unload(self):
+        for texture in self._textures.values():
+            RL.unload_texture(texture)
+
+        self._textures.clear()
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pyray as RL
+from ResourceManager import ResourceManager
 
 
 class DroneFighter:
-    SCREEN_WIDTH = 800
-    SCREEN_HEIGHT = 450
+    SCREEN_WIDTH = 1280
+    SCREEN_HEIGHT = 720
     TITLE = "Drone Fighter"
 
-    # Init game window  
+    # Init game window
     def __init__(self):
         print("Init window")
 
         RL.init_window(self.SCREEN_WIDTH, self.SCREEN_HEIGHT, self.TITLE)
         RL.set_target_fps(60)
+
+        self.resources = ResourceManager()
     
     # update
     def update(self):
@@ -22,12 +25,13 @@ class DroneFighter:
         RL.begin_drawing()
 
         RL.clear_background(RL.BLACK)
-        RL.draw_text(self.TITLE, 280, 200, 40, RL.WHITE)
-
+        RL.draw_texture(self.resources.get_texture("Starfield"), -50, -50, RL.WHITE)
+        RL.draw_text(self.TITLE, 500, 350, 40, RL.WHITE)
         RL.end_drawing()
 
     # exit game
     def exit_game(self):
+        self.resources.unload()
         RL.close_window()
     
     # start game
