b8269247d1a9b0688e9a65d73ef1aa4655012a5a
Robert Vari <mail.robertvari@gmail.com>
2026-07-09 19:58:10 +0200

    Spaceship base class

--- DF_Base.py
+++ DF_Base.py
@@ -1,4 +1,6 @@
+import pyray as RL
 from abc import ABC, abstractmethod
+from ResourceManager import ResourceManager
 
 class GameObject(ABC):
     @abstractmethod
@@ -8,3 +10,22 @@ class GameObject(ABC):
     @abstractmethod
     def draw(self):
         print("GameObject: draw")
+
+
+class Spaceship(GameObject):
+    SPEED = 200
+    SCALE = 3
+    ROTATION = 0
+    SOURCE_RECT = (32, 64, 16, 16)
+    TEXTURE_NAME = "Ships"
+
+    def __init__(self, resource_manager: ResourceManager):
+        self._texture = resource_manager.get_texture(self.TEXTURE_NAME)
+        self._source_rect = RL.Rectangle(*self.SOURCE_RECT)
+        self._size = self._source_rect.width * self.SCALE
+        self._origin = RL.Vector2(self._size / 2, self._size / 2)
+        self.position = RL.Vector2(RL.get_screen_width() / 2, RL.get_screen_height() / 2)
+
+    def draw(self):
+        dest_rect = RL.Rectangle(self.position.x, self.position.y, self._size, self._size)
+        RL.draw_texture_pro(self._texture, self._source_rect, dest_rect, self._origin, self.ROTATION, RL.WHITE)
--- Player.py
+++ Player.py
@@ -1,18 +1,9 @@
 import pyray as RL
-from DF_Base import GameObject
+from DF_Base import Spaceship
 from ResourceManager import ResourceManager
-from DF_Base import GameObject
 
-class Player(GameObject):
-    SPEED = 200
-    SCALE = 3
-
-    def __init__(self, resource_manager: ResourceManager):
-        self._texture = resource_manager.get_texture("Ships")
-        self._source_rect = RL.Rectangle(32, 64, 16, 16)
-        self._size = self._source_rect.width * self.SCALE
-        self._origin = RL.Vector2(self._size/2, self._size/2)
-        self.position =RL.Vector2(RL.get_screen_width()/2, RL.get_screen_height()/2)
+class Player(Spaceship):
+    ROTATION = 180
 
     def update(self):
         # Move spaceship
@@ -32,7 +23,3 @@ class Player(GameObject):
         direction = RL.vector2_normalize(RL.Vector2(dx, dy))
         self.position.x += direction.x * self.SPEED * dt
         self.position.y += direction.y * self.SPEED * dt
-
-    def draw(self):
-        dest_rect = RL.Rectangle(self.position.x, self.position.y, self._size, self._size)
-        RL.draw_texture_pro(self._texture, self._source_rect, dest_rect, self._origin, 180, RL.WHITE)
