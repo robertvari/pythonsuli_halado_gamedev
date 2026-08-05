85a59b52651c8a3c57ad98750d45976acb58d7a7
Robert Vari <mail.robertvari@gmail.com>
2026-08-05 16:32:38 +0200

    Track player

--- Enemy.py
+++ Enemy.py
@@ -1,23 +1,25 @@
 from DF_Base import Spaceship
 from ResourceManager import ResourceManager
+from Player import Player
 import pyray as RL
 from abc import abstractmethod
 from random import randint
 
 class Enemy(Spaceship):
+    ROTATION = -90
     SOURCE_RECT = (64, 64, 16, 16)
 
-    def __init__(self, resource_manager: ResourceManager, spawn_position: RL.Vector2):
+    def __init__(self, resource_manager: ResourceManager, spawn_position: RL.Vector2, player: Player):
         super().__init__(resource_manager)
         self.position = spawn_position
-
+        self._player = player
 
     @abstractmethod
     def update(self, dt: float):
         pass
 
 class EnemySpawner:
-    def __init__(self, resource_manager: ResourceManager, enemy_count: int):
+    def __init__(self, resource_manager: ResourceManager, enemy_count: int, player: Player):
         self.enemies: list[Enemy] = []
 
         for i in range(enemy_count):
@@ -27,7 +29,8 @@ class EnemySpawner:
                     RL.Vector2(
                         randint(100, RL.get_screen_width()-100),
                         randint(100, RL.get_screen_height()-100)
-                    )
+                    ),
+                    player
                 )
             )
 
@@ -41,4 +44,4 @@ class EnemySpawner:
 
 class Drone(Enemy):
     def update(self, dt: float):
-        pass
+        self.rotate_to(self._player.position)
